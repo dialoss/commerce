9b34a1c1ec55361ce38b4e95b21c58da052657b4
Alex <dialoss775@gmail.com>
2024-04-06 18:31:07 +0300

    Updates

--- static/js/508.8c587561.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcommerce=self.webpackChunkcommerce||[]).push([[508],{22508:(e,c,s)=>{s.r(c),s.d(c,{default:()=>u});const u=s(36470).A}}]);
-//# sourceMappingURL=508.8c587561.chunk.js.map
---
+++ static/js/508.fd24bdc7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcommerce=self.webpackChunkcommerce||[]).push([[508],{22508:(e,c,s)=>{s.r(c),s.d(c,{default:()=>u});const u=s(65981).A}}]);
+//# sourceMappingURL=508.fd24bdc7.chunk.js.map
