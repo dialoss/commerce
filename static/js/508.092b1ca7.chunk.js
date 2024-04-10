7463f50a3bc96f554aeeddaab8925d74db36138d
Alex <dialoss775@gmail.com>
2024-04-11 02:09:03 +0300

    Updates

--- static/js/508.092b1ca7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcommerce=self.webpackChunkcommerce||[]).push([[508],{22508:(e,c,s)=>{s.r(c),s.d(c,{default:()=>u});const u=s(75918).A}}]);
-//# sourceMappingURL=508.092b1ca7.chunk.js.map
